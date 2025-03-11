08960f129825757f59ca875749dadc8b155d310a
Will Thompson <wjt@endlessos.org>
2025-03-11 14:39:09 +0000

    ExtraWorldSelector: Move header & back button out of scrolled list
    Now that the “Community Worlds” heading is smaller, there is enough
    space in the page for only the list of worlds to scroll.
    
    Move the heading out of the scrollable area, leaving it at the top of
    the page. Move the back button out of the scrollable area, moving it to
    the bottom of the page.
    
    Explicitly set the focus neighbour properties so that pressing "Up" from
    the Back button focuses the last world (rather than the last visible
    world in the list); and that pressing "Down" from the Back button
    focuses the first world (rather than doing nothing). Similarly, make
    pressing "Up" from the first world wrap around to the Back button at the
    bottom.
    
    Set the focus_next/_previous properties such that pressing (Ctrl+)Tab
    jumps between the list of worlds and the Back button, rather than
    between the buttons in the list (which can be focused with the arrow
    keys or D-pad).
    
    Fixes https://github.com/endlessm/candy-collective/issues/127

--- Script/WorldSelector/ExtraWorldSelector.gd
+++ Script/WorldSelector/ExtraWorldSelector.gd
@@ -35,13 +35,33 @@ func _find_worlds(path) -> Array[String]:
 
 func _ready() -> void:
 	var worlds := _find_worlds(WORLDS_DIR)
+	var first_button: Button
+	var button: Button
 
 	for world in worlds:
-		var button = Button.new()
+		button = Button.new()
 		button.text = world
 		button.pressed.connect(_on_world_button_pressed.bind(WORLDS_DIR.path_join(world).path_join("World.tscn")))
 		ExtraWorldsBox.add_child(button)
+		
+		if not first_button:
+			first_button = button
 
+		# Tab or Ctrl+Tab from a button jumps to the Back button
+		button.focus_next = BackButton.get_path()
+		button.focus_previous = BackButton.get_path()
+
+	# Ensure that, when the back button is focused, pressing "up" goes to the
+	# last world in the list, rather than the last world visible in the current
+	# scroll viewport
+	BackButton.focus_neighbor_top = button.get_path()
+	button.focus_neighbor_bottom = BackButton.get_path()
+	
+	# Make the list wrap around so that pressing "up" from the first world
+	# focuses the Back button below the list, and pressing "down" on the Back
+	# button focuses the first world.
+	first_button.focus_neighbor_top = BackButton.get_path()
+	BackButton.focus_neighbor_bottom = first_button.get_path()
 
 func _on_world_button_pressed(world: String) -> void:
 	world_selected.emit(world)
